a101ec8fb6ab1352d2c39288943e6b136d5f7778
Artyom Levykh <art.lv@hotmail.com>
2021-11-28 21:14:57 +0200

    class Worker:

---
+++ класс-контейнер.py
@@ -0,0 +1,8 @@
+class Worker:
+    def __init__(self, surname, name, patronymic, year, prof):              # Атрибуты класса Worker
+        self.surname = surname
+        self.name = name
+        self.patronymic = patronymic
+        self.year = year
+        self.prof = prof
+
