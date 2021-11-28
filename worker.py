43643122e6994cef64d39bd0d9c8fef53c8d21da
Artyom Levykh <art.lv@hotmail.com>
2021-11-28 21:51:34 +0200

    a-идентификатор объекта

--- worker.py
+++ worker.py
@@ -1,6 +1,6 @@
 class Worker:
     def __init__(self, surname, name, patronymic, year, prof):              # Атрибуты класса Worker
-        self.surname = surname
+        self.surname = surname                                              # Набор полей (атрибутов) с разными значениями
         self.name = name
         self.patronymic = patronymic
         self.year = year
