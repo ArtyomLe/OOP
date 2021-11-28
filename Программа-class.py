2f9fdd69fe72e475b42eeb6be00bf4adc4fc2e37
Artyom Levykh <art.lv@hotmail.com>
2021-11-28 16:00:54 +0200

    class Experiment:

--- Программа-class.py
+++ Программа-class.py
@@ -7,11 +7,11 @@ if (a % b == 0):
     print(f"{a} делится на {b}")
 else:
     print(f"{a} не делится на {b}")
-"""
+
 
 # Оформляем данный процесс как класс
 class Divisibility:
-    def __init__(self):
+    def __init__(self):                             # Конструктор класса (Чтобы вызывать метод без . )
         a = int(input("Введите первое число: "))
         b = int(input("Введите второе число: "))
         if a % b == 0:
@@ -21,3 +21,16 @@ class Divisibility:
 # Создаём(вызываем) ОБЪЕКТ а основе класса
 if __name__ == '__main__':
     Divisibility()
+
+"""
+
+class Experiment:
+    def run(self):
+        a = int(input("Введите первое число: "))
+        b = int(input("Введите второе число: "))
+        if (a % b == 0):
+            print(f"{a} делится на {b}")
+        else:
+            print(f"{a} не делится на {b}")
+if __name__ == '__main__':
+    Experiment().run()
