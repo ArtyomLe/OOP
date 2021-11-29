c4ab533be384035d7d8ec46de5fd0245e6d5583f
Artyom Levykh <art.lv@hotmail.com>
2021-11-29 10:09:17 +0200

    grid список

--- mainmeatgrinder.py
+++ mainmeatgrinder.py
@@ -1,4 +1,10 @@
 from meatGrinder import MeatGrinder
 
 grid = []
-grid.append()
+grid.append(MeatGrinder("Brass", 80, 3**2 + 4**2 + 5, "polygon"))
+grid.append(MeatGrinder("Steel", 86, 9, "X"))
+grid.append(MeatGrinder("Plastic", 60, 7, "+"))
+grid.append(MeatGrinder("Silver", 82, 9, "*"))
+
+for g in grid:
+    print(f"{g.material}, {g.size}, {g.holes}, {g.figure}")
