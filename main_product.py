048981636d3b493a11ab38f7bd85b6142e703234
Artyom Levykh <art.lv@hotmail.com>
2021-11-29 20:27:30 +0200

    Основное тело программы

--- main_product.py
+++ main_product.py
@@ -1,12 +1,12 @@
 from product import Product
 
-products = [
-    Product("Болты и гайки", 5, 500),
-    Product("Зубные щётки", 20, 100),
-    Product("Мясорубки", 750, 300),
-    Product("Насадки для пылесоса", 2100, 150),
-    Product("Футляры", 1000, 10),
-    Product("Учебники по Python", 1100, 50)
+products = [                                        # Шесть экземпляров
+    Product("Болты и гайки", 5, 500),               # 0
+    Product("Зубные щётки", 20, 100),               # 1
+    Product("Мясорубки", 750, 300),                 # 2
+    Product("Насадки для пылесоса", 2100, 150),     # 3
+    Product("Футляры", 1000, 10),                   # 4
+    Product("Учебники по Python", 1100, 50)         # 5
     ]
 
 def printHead(s): # Заголовок
@@ -17,8 +17,8 @@ def printHead(s): # Заголовок
 def printList(): # Содержимое трюма в виде таблицы
     printHead("СТАТИСТИКА:")
     for i in range(len(products)):
-        print(f"{(i + 1):3}. {products[i]}") # Возвращаемая строка указана в методе __str__ класса
-
+        print(f"{(i + 1):3}. {products[i]}") # {products[i]} возвращаемая строка указана в методе __str__ класса
+                                             # {(i + 1):3}. Цифры начинающиеся с 1 а не с 0, (:3 три пробела от левого края) }. точка после цифры
 def sumAll(): # Возвращает общую сумму
     total_cost = 0
     for p in products:
@@ -38,5 +38,5 @@ while playGame:
     if n == 0:
         playGame = False
     elif n > 0 and n <= len(products):
-        products[n - 1].menu()
+        products[n - 1].menu()          # Вызываем меню для продукта n
 print("Успешного полёта, капитан!")
