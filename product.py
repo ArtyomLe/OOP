76e503a864e5822933cf4329c6b6feb222467cbb
Artyom Levykh <art.lv@hotmail.com>
2021-11-29 16:00:52 +0200

    Пояснения методов в классе Product

--- product.py
+++ product.py
@@ -10,34 +10,34 @@ class Product:
     def __str__(self):
         return f"{self.name:22} Кол-во: {str(self.count):10} Цена: {str(self.price):10} СУММА: {str(self.getPrice()):20}"
 
-    def printCurrent(self):
+    def printCurrent(self): # Вывод строки для каждого товара
         print(f"\nТовар: {self.name.upper()}, количество: {self.count}")
 
-    def add(self, adding):
+    def add(self, adding): # Метод добавляющий товар
         print(f"Добавленно {adding} единиц {self.name}")
         self.count += adding
 
-    def adding(self):
+    def adding(self): # Запрос необходимого для добавления кол-ва товара и вызывающий (def add) с введённым пользователем аргументом
         self.printCurrent()
         self.add(int(input("Введите, сколько товара добавить (0 - выход): ")))
 
-    def extract(self, ext):
+    def extract(self, ext): # Метод выгружающий товар (вычитает из count указанное аргументом ext количество)
         if ext > self.count:
             print(f"Невозомжно разгрузить {ext} единиц {self.name} (остаток: {self.count})")
             return False
         self.count -= ext
         return True
 
-    def extracting(self):
+    def extracting(self): # Метод передающий в (def extract) через переменную а кол-во товара для разгрузки
         self.printCurrent()
         a = int(input("Сколько товара выгрузить (0 - выход): "))
-        if (not self.extract(a)):
+        if (not self.extract(a)): # В случае если (def extract) возвращает False (просим больше чем есть в наличии) то получаем рекурсию
             self.extracting()
 
-    def getPrice(self):
+    def getPrice(self): # Метод возвращающий общую сумму товара (кол-во x цена)
         return self.count * self.price
 
-    def menu(self):
+    def menu(self): # Метод формирующий меню и обрабатывающий ввод пользователя
         self.printCurrent()
         print("1. Загрузить")
         print("2. Выгрузить")
