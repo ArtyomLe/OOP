b2b904c86796925b5751226ff70a45bd890bb2f6
Artyom Levykh <art.lv@hotmail.com>
2021-11-29 14:37:50 +0200

    Product

--- mainmeatgrinder.py
+++ mainmeatgrinder.py
@@ -1,7 +1,7 @@
 from meatGrinder import MeatGrinder
 
 grid = []
-grid.append(MeatGrinder("Brass", 80, 3**2 + 4**2 + 5, "polygon"))
+grid.append(MeatGrinder("Brass", 80, 3*2 + 4*2 + 5, "polygon"))
 grid.append(MeatGrinder("Steel", 86, 9, "X"))
 grid.append(MeatGrinder("Plastic", 60, 7, "+"))
 grid.append(MeatGrinder("Silver", 82, 9, "*"))
