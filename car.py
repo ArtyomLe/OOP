c31d08ad664dd63907075c86075f5a88dafece74
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-11-30 08:49:35 +0200

    Delete car.py

--- car.py
+++
@@ -1,7 +0,0 @@
-class Car:
-
-    def __init__(self, name, year, color, form):
-        self.name = name    # Марка
-        self.year = year    # Год выпуска
-        self.color = color  # Цвет
-        self.form = form    # Форм фактор
