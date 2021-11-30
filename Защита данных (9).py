4709008fa2e5a2f72bd5d6f5da821d8777fd42a8
Artyom Levykh <art.lv@hotmail.com>
2021-11-30 12:07:43 +0200

    Перевод числа из троичной в десятичную систему(шифр)

--- Защита данных (9).py
+++ Защита данных (9).py
@@ -1,14 +1,13 @@
-cString = "НУПРИВЕТ"
+cString = "@#$"                    # Троичная система счисления
 
-n = 97
-base = len(cString)
-res = ""
+n = "$$$#"                         #  2221 в троичной системе счисления ($ = 2, # = 1)
+base = len(cString)                # 3
+res = 0
 
-if n == 0:
-    res = cString[0]
-else:
-    while n > 0:
-        res = cString[n % base] + res
-        n //= base
-
+for i in range(len(n)):
+    res = res * base + cString.find(n[i])   # res = 0 * 3 + 2 = 2
+                                            # res = 2 * 3 + 2 = 8
+                                            # res = 8 * 3 + 2 = 26
+                                            # res = 26 * 3 + 1 = 79
 print(res)
+# Результат: 79 (Число 2221 в троичной системе счисления равно 79 в десятичной)
