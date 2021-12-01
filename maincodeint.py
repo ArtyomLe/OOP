d48ec07cbec8759e2605f12286d704a0b3a9d039
Artyom Levykh <art.lv@hotmail.com>
2021-12-01 22:38:54 +0200

    Начало ряда Фибоначи

--- maincodeint.py
+++ maincodeint.py
@@ -1,7 +1,22 @@
 
 from codeint import CodeInt
 
-a = CodeInt(10)
-a = 10 + a
-print(a)
+print("Десятичная форма числа")
+a = CodeInt(0)
+b = CodeInt(1)
+print(f"{a} {b}", end=" ")
+for i in range(20):
+    c = a + b
+    print(c, end=" ")
+    a = b
+    b = c
 
+print("\nВнутренняя форма числа")
+a = CodeInt(0)
+b = CodeInt(1)
+print(f"{a.digit} {b.digit}", end=" ")
+for i in range(20):
+    c = a + b
+    print(c.digit, end=" ")
+    a = b
+    b = c
